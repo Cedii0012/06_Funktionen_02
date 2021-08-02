893158e0fe39cc6f2f6577f9ca21773d958bf28c
Peter <schmidt.multimedia@gmail.com>
2021-08-02 10:45:47 +0200

    03 / Modul: Subtraktion a - b

--- script_01.js
+++ script_01.js
@@ -8,13 +8,18 @@
 4. Ausgabe in Konsole : check!
 */
 
+// Modul: Subtraktion a - b |  Test:
+ausgabe(subtrahieren(2,1));
+ausgabe(subtrahieren(2,10));
+function subtrahieren(a,b) {
+    return a - b;
+}
 
 // Modul: Addition a + b |  Test:
-ausgabe(addieren(2,1));
+// ausgabe(addieren(2,1));
 function addieren(a,b) {
     return a + b;
 }
-
 
 // Modul: Konsolenausgabe |  Test:
 // ausgabe("Hallo Welt!")
